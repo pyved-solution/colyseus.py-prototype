fefa5f38ce116ffb0937872c098c28740846a695
wkta <thomas.iw@kata.games>
2024-10-21 17:52:30 +0200

    import initial codebase

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # colyseus.py-prototype
-own study of the colyseus state sync mechanism, mostly in python &amp; attempt to use ws for interoperability
+
+In this repo you will find my own study of the colyseus state sync mechanism, mostly in python &amp; attempt to use ws for interoperability. For most people this won't be useful as this is too experimental.
+
+## Getting started
+
+- if you wish to perform online analysis, in general `npm install` followed by `npm run start` is enough.
+
+## A few ideas
+
+- use an ancient version to avoid the "schema" serializer for easier prototyping using py
+- fork the current ColyseusJS and modify it to use plain JS objects for state synchronization
+- use a schema generator for py that produces dataclasses only
+
+## Useful links
+
+Old releases of Colyseus:
+- https://github.com/colyseus/colyseus/releases?page=2
+I don't know at what point the "schema" serializer has been introducted
+
+Alternatives to the schema serializer:
+- https://www.npmjs.com/package/msgpackr
---
+++ online-analysis/client/index.ts
@@ -0,0 +1,152 @@
+import Phaser from "phaser";
+import { Client, Room } from "colyseus.js";
+
+// custom scene class
+export class GameScene extends Phaser.Scene {
+    client = new Client("ws://localhost:2567");
+    room: Room;
+    playerEntities: { [sessionId: string]: any } = {};
+    currentPlayer: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+    remoteRef: Phaser.GameObjects.Rectangle;
+
+    // local input cache
+    inputPayload = {
+        left: false,
+        right: false,
+        up: false,
+        down: false,
+    };
+
+    cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+
+    preload() {
+        // preload scene
+        this.load.image('ship_0001', 'https://cdn.glitch.global/3e033dcd-d5be-4db4-99e8-086ae90969ec/ship_0001.png');
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
+
+    }
+
+
+
+    async create() {
+
+        console.log("Joining room...");
+
+        try {
+            this.room = await this.client.joinOrCreate("my_room");
+            console.log("Joined successfully!");
+
+            this.room.state.players.onAdd((player, sessionId) => {
+                const entity = this.physics.add.image(player.x, player.y, 'ship_0001');
+                this.playerEntities[sessionId] = entity;
+
+                if (sessionId === this.room.sessionId) {
+                    // this is the current player!
+                    // (we are going to treat it differently during the update loop)
+                    this.currentPlayer = entity;
+
+                    // remoteRef is being used for debug only
+                    this.remoteRef = this.add.rectangle(0, 0, entity.width, entity.height);
+                    this.remoteRef.setStrokeStyle(1, 0xff0000);
+
+                    player.onChange(() => {
+                        this.remoteRef.x = player.x;
+                        this.remoteRef.y = player.y;
+                    });
+
+                } else {
+                    // all remote players are here!
+                    // (same as before, we are going to interpolate remote players)
+                    player.onChange(() => {
+                        entity.setData('serverX', player.x);
+                        entity.setData('serverY', player.y);
+                    });
+                }
+            });
+
+            this.room.state.players.onRemove((player, sessionId) => {
+                const entity = this.playerEntities[sessionId];
+                if (entity) {
+                    // destroy entity
+                    entity.destroy();
+
+                    // clear local reference
+                    delete this.playerEntities[sessionId];
+                }
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    fixedTick(time: number, delta: number): void {
+        // skip loop if not connected yet.
+        if (!this.currentPlayer) { return; }
+    
+        const velocity = 2;
+        this.inputPayload.left = this.cursorKeys.left.isDown;
+        this.inputPayload.right = this.cursorKeys.right.isDown;
+        this.inputPayload.up = this.cursorKeys.up.isDown;
+        this.inputPayload.down = this.cursorKeys.down.isDown;
+        this.room.send(0, this.inputPayload);
+    
+        if (this.inputPayload.left) {
+            this.currentPlayer.x -= velocity;
+    
+        } else if (this.inputPayload.right) {
+            this.currentPlayer.x += velocity;
+        }
+    
+        if (this.inputPayload.up) {
+            this.currentPlayer.y -= velocity;
+    
+        } else if (this.inputPayload.down) {
+            this.currentPlayer.y += velocity;
+        }
+
+        for (let sessionId in this.playerEntities) {
+            // do not interpolate the current player
+            if (sessionId === this.room.sessionId) {
+                continue;
+            }
+        
+            // interpolate all other player entities
+            const entity = this.playerEntities[sessionId];
+            const { serverX, serverY } = entity.data.values;
+        
+            entity.x = Phaser.Math.Linear(entity.x, serverX, 0.2);
+            entity.y = Phaser.Math.Linear(entity.y, serverY, 0.2);
+        }
+    }
+
+    elapsedTime = 0;
+    fixedTimeStep = 1000 / 60;
+    
+    update(time: number, delta: number): void {
+        // skip loop if not connected yet.
+        if (!this.currentPlayer) { return; }
+    
+        this.elapsedTime += delta;
+        while (this.elapsedTime >= this.fixedTimeStep) {
+            this.elapsedTime -= this.fixedTimeStep;
+            this.fixedTick(time, this.fixedTimeStep);
+        }
+    }
+
+
+}
+
+// game config
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#b6d53c',
+    parent: 'phaser-example',
+    physics: { default: "arcade" },
+    pixelArt: true,
+    scene: [GameScene],
+};
+
+// instantiate the game
+const game = new Phaser.Game(config);
